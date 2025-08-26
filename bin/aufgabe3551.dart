8d4654920d40cc8c3bb5d995938bb104ac0c600a
Daniel Trompetter <danieltrompetter@gmail.com>
2025-08-26 11:42:08 +0200

    Added Bonus

--- bin/aufgabe3551.dart
+++ bin/aufgabe3551.dart
@@ -1,9 +1,8 @@
 void main() 
 {
-  for(int i=1;i<=100;i++)
-  {
-    switch(i)
-    {
+  // Aufgabe 1
+  for(int i=1;i<=100;i++){
+    switch(i) {
       case <10:
         print("Kleine Zahl: $i");
       case >=10 && <60:
@@ -12,4 +11,29 @@ void main()
         print("Große Zahl: $i");
     }
   }
+
+  // bonus
+  String word = "otto";  
+  bool isPalindrom = true;
+
+  int reverseCounter = word.length;
+  for(int i=0;i<word.length ~/2;i++)
+  {
+      reverseCounter--;
+      if(word[i] != word[reverseCounter])
+      {
+        isPalindrom = false;
+      }
+  }
+
+  // Hier deine for-Schleife:
+  if (isPalindrom) 
+  {
+    print('$word ist ein Palindrom');
+  } 
+  else 
+  {
+    print('$word ist kein Palindrom');
+  }
 }
+
