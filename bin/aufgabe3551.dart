bc8372149f2152afbffa80335c3e30a238877c46
Daniel Trompetter <danieltrompetter@gmail.com>
2025-08-26 11:18:59 +0200

    init

---
+++ bin/aufgabe3551.dart
@@ -0,0 +1,15 @@
+void main() 
+{
+  for(int i=1;i<=100;i++)
+  {
+    switch(i)
+    {
+      case <10:
+        print("Kleine Zahl: $i");
+      case >=10 && <60:
+        print("Mittlere Zahl: $i");
+      default:
+        print("Große Zahl: $i");
+    }
+  }
+}
